eb2c56bb96264af0d2c48590c962952727f269ad
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-17 11:32:12 +0100

    Add Feedback

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -0,0 +1,46 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Feedback {
+    private String attempt;
+    List<Mark> marks;
+
+    public Feedback(String attempt, List<Mark> marks) {
+        this.attempt = attempt;
+        this.marks = marks;
+    }
+
+    public boolean isWordGuessed() {
+        return marks.stream()
+                .allMatch( mark -> mark == Mark.CORRECT);
+    }
+
+    public boolean isWordValid() {
+        return marks.stream()
+                .noneMatch(mark -> mark == Mark.INVALID);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feedback feedback = (Feedback) o;
+        return Objects.equals(attempt, feedback.attempt) &&
+                Objects.equals(marks, feedback.marks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attempt, marks);
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "attempt='" + attempt + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
