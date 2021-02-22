8d5d0a747957a80666f1bbdedf3bef2b8593f445
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-22 11:52:59 +0100

    Add Turn and TurnTest

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Turn.java
@@ -0,0 +1,68 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Turn {
+    private Feedback feedback;
+
+    public Turn() {
+        this.feedback = null;
+    }
+
+    public Feedback getFeedback() { return feedback; }
+
+    public Feedback takeGuess(String word, String guess) {
+        if (guess == null || word == null) throw new IllegalArgumentException("Word and Guess cannot be null");
+        List<Mark> marks;
+        if (isGuessAndWordSameLength(word, guess)){
+            marks = generateMarks(word, guess);
+        }else{
+            marks = generateInvalidMarks(guess);
+        }
+        this.feedback = new Feedback(guess, marks);
+        return this.feedback;
+    }
+
+    private boolean isGuessAndWordSameLength(String word, String guess) {
+        return word.length() == guess.length();
+    }
+
+    private List<Mark> generateMarks(String guess, String word) {
+        List<Mark> marks = new ArrayList<>();
+        char[] wordChars = word.toCharArray();
+        for (int index = 0; index < wordChars.length; index++){
+            Mark mark = calculateMark(word, index, guess);
+            marks.add(index, mark);
+        }
+        return marks;
+    }
+
+    private Mark calculateMark(String word, int index, String guess) {
+        char[] guessChars = guess.toCharArray();
+        if (isCharOnIndex(word, index, guessChars[index])){
+            return Mark.CORRECT;
+        }else if(isCharacterContainedWord(word, guessChars[index])){
+            return Mark.PRESENT;
+        }else {
+            return Mark.ABSENT;
+        }
+    }
+
+    private boolean isCharOnIndex(String word, int index, char character) {
+        return word.charAt(index) == character;
+    }
+
+    private boolean isCharacterContainedWord(String word, char character) {
+        return word.contains(String.valueOf(character));
+    }
+
+    private List<Mark> generateInvalidMarks(String guess) {
+        List<Mark> marks = new ArrayList<>();
+        for (int i = 0; i < guess.toCharArray().length; i++){
+            marks.add(Mark.INVALID);
+        }
+        return marks;
+    }
+
+}
---
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/TurnTest.java
@@ -0,0 +1,108 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TurnTest {
+    private  Turn turn= new Turn();
+
+    @BeforeEach
+    void setUp() {
+        this.turn = new Turn();
+    }
+
+    static Stream<Arguments> provideTurnGuesses() {
+        return Stream.of(
+                Arguments.of(
+                        "woord",
+                        "woord"
+                ),Arguments.of(
+                        "woord",
+                        "marge"
+                ),Arguments.of(
+                        "woord",
+                        "waden"
+                ),Arguments.of(
+                        "woorde",
+                        "woorde"
+                ),Arguments.of(
+                        "woorde",
+                        "marges"
+                ),Arguments.of(
+                        "woorde",
+                        "wadden"
+                ),Arguments.of(
+                        "woorden",
+                        "woorden"
+                ),Arguments.of(
+                        "woorden",
+                        "magiers"
+                ),Arguments.of(
+                        "woorden",
+                        "waddens"
+                )
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideTurnGuesses")
+    @DisplayName("mMake correct and non invalid guesses")
+    void makeNonInvalidGuesses(String word, String guess) {
+        turn.takeGuess(word, guess);
+        assertNotNull(turn.getFeedback());
+    }
+
+    static Stream<Arguments> provideInvalidSizedTurnGuesses() {
+        return Stream.of(
+                Arguments.of(
+                        "woord",
+                        "woorde",
+                        List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID)
+                ),Arguments.of(
+                        "woord",
+                        "marg",
+                        List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID)
+                ),Arguments.of(
+                        "woord",
+                        "",
+                        List.of()
+                )
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideInvalidSizedTurnGuesses")
+    @DisplayName("Invalid guesses return Turn with feedback with INVALID marks")
+    void makeInvalidGuesses(String word, String guess, List<Mark> expected) {
+        turn.takeGuess(word, guess);
+        assertEquals(
+                turn.getFeedback().marks,
+                expected
+        );
+    }
+
+    @Test
+    @DisplayName("Make null as guess")
+    void nullAsGuess() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> turn.takeGuess("woord", null)
+        );
+    }
+
+    @Test
+    @DisplayName("Make guess while word is null")
+    void nullAsWord() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> turn.takeGuess(null, "guess")
+        );
+    }
+}
