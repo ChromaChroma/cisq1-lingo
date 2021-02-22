e11a2c0c245cab2f95d5f71b202da2ac61cad08e
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-02-22 11:51:14 +0100

    Add string constructor

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidHintException.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidHintException.java
@@ -1,4 +1,10 @@
 package nl.hu.cisq1.lingo.trainer.domain.exception;
 
 public class InvalidHintException extends RuntimeException{
+
+    public InvalidHintException() { }
+
+    public InvalidHintException(String message) {
+        super(message);
+    }
 }
