f6be934c1b1d80cb05f134173f391812daa9e915
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-03-15 13:21:18 +0100

    Add feedback exception

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,13 +1,12 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
-import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidFeedbackException;
+import nl.hu.cisq1.lingo.trainer.exception.InvalidFeedbackException;
 
 import java.util.List;
-import java.util.Objects;
 
 public class Feedback {
     private String attempt;
-    List<Mark> marks;
+    private List<Mark> marks;
 
     public Feedback(String attempt, List<Mark> marks) {
         if (attempt.length() != marks.size()) throw new InvalidFeedbackException("Guess and marks lengths do not match");
@@ -24,25 +23,5 @@ public class Feedback {
         return Hint.of(previousHint, attempt, marks);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Feedback feedback = (Feedback) o;
-        return Objects.equals(attempt, feedback.attempt) &&
-                Objects.equals(marks, feedback.marks);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(attempt, marks);
-    }
-
-    @Override
-    public String toString() {
-        return "Feedback{" +
-                "attempt='" + attempt + '\'' +
-                ", marks=" + marks +
-                '}';
-    }
+    public List<Mark> getMarks() { return marks; }
 }
